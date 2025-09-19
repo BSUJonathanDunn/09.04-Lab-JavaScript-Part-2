b272d1c6fd17f8afb3bf1ab536957669a8c5b563
Jonathan Dunn <jonathandunn@u.boisestate.edu>
2025-09-19 11:06:37 -0600

    new file:   dist/css/style.css 	new file:   dist/fonts/Cagliostro-Regular.ttf 	new file:   dist/fonts/EurostarRegular.ttf 	new file:   dist/fonts/Final-Fantasy-36-Font.woff 	new file:   dist/fonts/Final-Fantasy-36-Font.woff2 	new file:   dist/fonts/Highwind-pndZ.ttf 	new file:   dist/fonts/PixelOperator-Bold.ttf 	deleted:    dist/funny.png 	new file:   dist/img/42.png 	new file:   dist/img/eyeclosed.png 	new file:   dist/img/eyeclosehover.png 	new file:   dist/img/eyeopen.png 	new file:   dist/img/eyeopenhover.png 	new file:   dist/img/gnome.png 	new file:   dist/js/script.js 	deleted:    dist/script.js 	new file:   dist/sound/FF14Distance.mp3 	new file:   dist/sound/FF6Terra.flac 	new file:   dist/sound/GuildWars2Relaxing.mp3 	new file:   dist/sound/ThemeOfProntera.flac 	new file:   dist/sound/UndertaleHome.ogg 	new file:   dist/sound/UndertaleWaterfall.ogg 	new file:   dist/sound/aeriths_theme.ogg 	deleted:    dist/style.css 	new file:   dist/vid/FF14Thule.mp4 	new file:   dist/vid/FF6.mp4 	new file:   dist/vid/FF7.webm 	new file:   dist/vid/GuildWars2.mkv 	new file:   dist/vid/ROProntera.mp4 	new file:   dist/vid/UndertaleFP.webm 	new file:   dist/vid/UndertaleWP.mp4 	modified:   index.html

---
+++ dist/js/script.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7d4e35d393d54a2841fe7f18c86d15def1eb21b53cb41e51bbba0820e514da84
+size 23260
--- server.js
+++ server.js
@@ -1,29 +1,3 @@
-/* Simple Node.js and Express application server for serving static assets.*/
-
-const express = require('express');
-const path = require('path');
-const port = process.env.PORT || 8080;
-const app = express();
-
-// Middleware to serve static files from the 'dist' directory
-// This directory should contain the built assets of your application
-// Ensure that the 'dist' directory exists and contains your static files.
-// You can have subdirectories in 'dist' for better organization.
-// For example, you might have 'dist/css', 'dist/js', etc.
-// The static files will be served at the root URL (e.g., http://localhost:8080/)
-app.use(express.static(__dirname + '/dist'));
-
-// serve index.html for the root path
-app.get("/", function (request, response) {
-  response.sendFile(path.resolve(__dirname, 'index.html'));
-});
-
-// example route to demonstrate server functionality
-// this can be accessed at http://localhost:8080/hello
-// and will respond with "Hello World, from the server!"
-app.get("/hello", function (request, response) {
-    response.send("Hello World, from the server!");
-});
-
-app.listen(port);
-console.log("server started on port " + port);
+version https://git-lfs.github.com/spec/v1
+oid sha256:f30ed5df6add07993c87b73f0aa31154091870ef88e3a296c438664ae8132747
+size 1151
