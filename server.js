e8e9ca8680231c6882cc0370e45a0c0e57b570ec
Jonathan Dunn <155767391+BSUJonathanDunn@users.noreply.github.com>
2025-10-20 22:27:02 +0000

    modified:   package-lock.json 	modified:   package.json 	modified:   server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f30ed5df6add07993c87b73f0aa31154091870ef88e3a296c438664ae8132747
-size 1151
+const lfsData = {
+  version: "https://git-lfs.github.com/spec/v1",
+  oid: "sha256:f30ed5df6add07993c87b73f0aa31154091870ef88e3a296c438664ae8132747",
+  size: 1151
+};
